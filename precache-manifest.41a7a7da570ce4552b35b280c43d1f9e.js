c5713823db31c770c9aee275946806d4e0651f7c
zadorojniiarsen <zadorojniiarsen@gmail.com>
2021-08-31 22:03:05 +0300

    Updates

---
+++ precache-manifest.41a7a7da570ce4552b35b280c43d1f9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123db34c4b2137d4dcfde8457d19e6a2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "18b81ef210d24e18381b",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b81ef210d24e18381b",
+    "url": "./static/js/main.8abfe5a5.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
